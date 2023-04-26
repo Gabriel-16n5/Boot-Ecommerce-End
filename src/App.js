f7859a6a8eeeb4eefbc87c6d14ea71a0821f9c44
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-26 20:49:15 -0300

    Arquivos base para a criação do projeto

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import styled from "styled-components"
+import ProductsPage from "./pages/ProductsPage"
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
+import { useState } from "react"
+import { gray } from "./constants/colors"
+import AuthContext from "./contexts/AuthContext"
+
+export default function App() {
+  const [token, setToken] = useState(localStorage.getItem())
+  const [userName, setUserName] = useState(localStorage.getItem())
+
+  return (
+    <PagesContainer>
+      <AuthContext.Provider value={{}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<SignUpPage />} />
+            <Route path="/" element={<ProductsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
+    </PagesContainer>
+  )
+}
+
+const PagesContainer = styled.main`
+  background-color:  ${gray};
+  width: calc(100vw - 50px);
+  max-height: 100vh;
+  padding: 25px;
+`
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import { red, gray } from "../constants/colors";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        // we need define a default settings
+    }
+    button {
+
+    }
+    h1 {
+
+    }
+    input {
+
+    }
+    form {
+
+    }
+    a {
+
+    }
+`
+
+export default GlobalStyle
