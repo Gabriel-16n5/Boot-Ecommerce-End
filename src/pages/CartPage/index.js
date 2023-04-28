74a99702aeee80729099dd35a259798e42b8aaa4
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-28 05:06:24 -0300

    feat:cart

--- src/pages/CartPage/index.js
+++ src/pages/CartPage/index.js
@@ -1,5 +1,6 @@
-import { CartContainer, FinalizeOrder, FinalizeOrderData } from "./styled";
+import { CartContainer, CartProducts, FinalizeOrder, FinalizeOrderData, ResumeContainer } from "./styled";
 import Forms from "../../hooks/Forms"
+import styled from "styled-components"
 export default function CartPage() {
     const { form, formHandler } = Forms({ cupon: "", shipping: "" })
   
@@ -9,40 +10,93 @@ export default function CartPage() {
     }
   
     return (
+    <PageContainer>
       <CartContainer>
+        <ResumeContainer>
+            <div>
+                <><h2>Resumo</h2></>
+                <span>
+                        <div>
+                            <h3>Subtotal</h3>
+                            <h4>Total</h4>
+                        </div>
+                        <div>
+                            <h3>R$82,24</h3>
+                            <h4>R$82,24</h4>
+                        </div>
+                </span> 
+                <div>
+                    <p>á vista</p>
+                    <h5>R$69,90</h5>
+                    <h3>no PIX com 15% desconto</h3>
+                    <h2>-</h2>
+                    <h1>R$82,24</h1>
+                    <h3>em até 6x de<h1>13,71</h1> sem juros no cartão</h3>
+                </div>
+            </div>
+        </ResumeContainer>
+        <CartProducts>
+                <div>
+                   <><h2>Products</h2></> 
+                    <span>
+                        <div>
+                            <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/p/g/pg-rds-bk011.jpg" alt="placeHolder"/>
+                            <p>- 1 +</p>
+                        </div>
+                        <div>
+                        <p>Rodizios de Cadeiras 75MM Pichau Soft, PG-RDS-BK01</p>
+                        <h3>SKU: PG-RDS-BK01</h3>
+                        <h4>1 x R$ 82,24</h4>
+                        </div>
+                    </span>
+                </div>
+            <button>Wipe Cart</button>
+        </CartProducts>
         <FinalizeOrder>
-            
             <form onSubmit={submitForm}>
             <FinalizeOrderData>
                 <div>
-                    <input
-                        required
-                        type="cupon"
-                        placeholder="Cupon"
-                        name="cupon"
-                        value={form.cupon}
-                        onChange={formHandler}
-                    />
-                    <button>apply</button>
+                    <div><h5>Discount coupons</h5></div>
+                    <span>
+                        <input
+                            required
+                            type="cupon"
+                            placeholder="Cupon"
+                            name="cupon"
+                            value={form.cupon}
+                            onChange={formHandler}
+                        />
+                        <button>Apply</button>
+                    </span>
                  </div>
             </FinalizeOrderData>
             <FinalizeOrderData>
                 <div>
-                    <input
-                        required
-                        minLength={3}
-                        type="shipping"
-                        placeholder="Postal Code"
-                        name="shipping"
-                        value={form.shipping}
-                        onChange={formHandler}
-                    />
-                <button>Calculate</button>
+                    <div><h5>Shipping and dates</h5></div>
+                    <span>
+                        <input
+                            required
+                            minLength={3}
+                            type="shipping"
+                            placeholder="Postal Code"
+                            name="shipping"
+                            value={form.shipping}
+                            onChange={formHandler}
+                        />
+                        <button>Calculate</button>
+                    </span>
                 </div>
             </FinalizeOrderData>
-            <button type="submit">Finalize Order</button>
-            </form>
-        </FinalizeOrder>
+                <button type="submit">Finalize Order</button>
+                </form>
+            </FinalizeOrder>
       </CartContainer>
+    </PageContainer>
     )
   }
+
+  const PageContainer = styled.main`
+  /* background-color:  "gray"; */
+  width: calc(100vw - 50px);
+  max-height: 100vh;
+`
--- src/pages/CartPage/styled.js
+++ src/pages/CartPage/styled.js
@@ -1,11 +1,238 @@
 import styled from "styled-components"
-import {blue, gray, green} from "../../constants/colors"
-export const CartContainer = styled.section`
+import {gray, green} from "../../constants/colors"
+export const CartContainer = styled.main`
+      padding: 25px;
+`
 
+export const ResumeContainer = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-bottom: 20px;
+    div {
+        background-color: #424242;
+    }
+    h1 {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 900;
+        font-size: 20px;
+        line-height: 20px;
+        color: red;
+        background-color: #424242;
+    }
+    p {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 20px;
+        color: ${green};
+        background-color: #424242;
+    }
+    h3 {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 20px;
+        color: #FFFFFF;
+        background-color: #424242;
+    }
+    h5 {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 900;
+        font-size: 20px;
+        line-height: 20px;
+        color: ${green};
+        background-color: #424242;
+    }
+    h4 {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 20px;
+        color: #FFFFFF;
+        background-color: #424242;
+    }
+    span {
+    display:flex;
+    justify-content: space-around;
+    background-color: #424242;
+    align-items: center;
+    width: 90vw;
+    height: 30vw;
+    }
+    h2 {
+        margin-left: 10px;
+        margin-top: 15px;
+        width: 90vw;
+        height: 10vw;
+        background-color: #424242;
+        display:flex;
+        justify-content: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 44px;
+        line-height: 32px;
+        align-items: center;
+        text-align: center;
+        color: #FFFFFF;
+    }
+button {
+    margin-bottom:20px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width: 30vw;
+    height: 10vw;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    background-color: ${gray};
+    font-size: 14px;
+    font-weight: 600;
+    color: white;
+    cursor: pointer;
+    margin-top: 40px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    }
+`
 
+export const CartProducts = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    padding-left: 300px;
+    div {
+        background-color: #424242;
+    }
+    p {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 20px;
+        color: #FFFFFF;
+        background-color: #424242;
+    }
+    h3 {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 20px;
+        color: #FFFFFF;
+        background-color: #424242;
+    }
+    h4 {
+        margin-left: 10px;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 20px;
+        color: #FFFFFF;
+        background-color: #424242;
+    }
+    img {
+        margin-left: 10px;
+        margin-right: 20px;
+        width: 120px;
+        height: 120px;
+    }
+    span {
+    display:flex;
+    background-color: #424242;
+    width: 90vw;
+    height: 50vw;
+    }
+    h2 {
+        margin-left: 10px;
+        width: 90vw;
+        height: 10vw;
+        background-color: #424242;
+        display:flex;
+        justify-content: start;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 32px;
+        align-items: center;
+        text-align: center;
+        color: #FFFFFF;
+    }
+button {
+    margin-bottom:10px;
+    align-items: center;
+    justify-content: center;
+    width: 30vw;
+    height: 10vw;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    background-color: ${gray};
+    font-size: 14px;
+    font-weight: 600;
+    color: white;
+    cursor: pointer;
+    margin-top: 20px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    }
 `
 
 export const FinalizeOrder = styled.section`
+    margin-top: 10px;
     button {
     outline: none;
     border: none;
@@ -16,36 +243,82 @@ export const FinalizeOrder = styled.section`
     color: white;
     cursor: pointer;
     padding: 12px;
+    width: 90vw;
     }
 `
 export const FinalizeOrderData = styled.section`
     display:flex;
-    border:box-sizing;
-    gap: 15px;
-    div{
+    padding-right: 10px;
+    h5 {
+    width: 90vw;
+    background-color: #424242;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 32px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #FFFFFF;
+    }
+    span {
     align-items:center;
-    justify-content: center;
+    justify-content: space-between;
     display:flex;
     border:box-sizing;
-    background-color: ${gray};
+    background-color: #424242;
     width: 90vw;
-    height: 20vw;
+    height: 35vw;
+    padding-left: 10px;
+    padding-right: 10px;
+    }
+    div{
+    display:flex;
+    flex-direction: column;
+    border:box-sizing;
+    background-color: #424242;
+    width: 90vw;
+    height: 35vw;
+    padding-left: 10px;
+    padding-right: 10px;
     }
     input {
-    width: 45vw;
+    margin-top: 40px;
+    width: 46vw;
     height: 3vw;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+    display: flex;
+    align-items: center;
+    text-align: start;
+    color: #FFFFFF;
+    margin-bottom:10px;
     }
     button {
-    width: 45vw;
-    height: 11vw;
+    margin-bottom:10px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width: 30vw;
+    height: 10vw;
     outline: none;
     border: none;
     border-radius: 5px;
-    background-color: ${blue};
+    background-color: ${gray};
     font-size: 20px;
     font-weight: 600;
     color: white;
     cursor: pointer;
     padding: 12px;
+    margin-top: 40px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
     }
 `
